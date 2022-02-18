2c64dbf9cede1252310798e33826907fe31cd9fb
yingchuw <yingchu.w@gmail.com>
2022-02-18 15:25:40 +0800

    update slider and RWD

--- project_js.js
+++ project_js.js
@@ -15,18 +15,4 @@ nav_close.addEventListener('click', function(){
     nav_body.classList.remove('js-active');
 })
 
-/*
-let nav_btn = document.getElementById('NavBtn');
-let nav_close = document.getElementById('NavClose');
-let nav_body = document.getElementById('NavBody');
-nav_btn.addEventListener('click', function () {
-    nav_btn.classList.toggle('js-none');
-    nav_close.classList.toggle('js-none');
-    nav_body.classList.add('js-active');
-})
-nav_close.addEventListener('click', function(){
-    nav_btn.classList.toggle('js-none');
-    nav_close.classList.toggle('js-none');
-    nav_body.classList.remove('js-active');
-})
-*/
+
