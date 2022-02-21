93bbd66a08a532ccff9caccb7f48f59204d3d062
yingchuw <yingchu.w@gmail.com>
2022-02-21 17:47:39 +0800

    update nav hamburger

--- project_js.js
+++ project_js.js
@@ -1,18 +1,60 @@
 "use strict";
 
-// hamberger btn
-let showmenu = document.getElementById('NavBtn');
-let nav_close = document.getElementById('NavClose');
-let menu = document.getElementById('NavBody');
-showmenu.addEventListener('click', function () {
-    showmenu.classList.toggle('js-none');
-    nav_close.classList.toggle('js-none');
-    menu.classList.add('js-active');
-})
-nav_close.addEventListener('click', function(){
-    nav_btn.classList.toggle('js-none');
-    nav_close.classList.toggle('js-none');
-    nav_body.classList.remove('js-active');
-})
+// go back to top btn
+mybutton = document.getElementById("topBtn"); //Get the button
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // for safari
+    document.documentElement.scrollTop = 0; // for chrome
+}
+
+// hambergur btn
+// jQuery 版本
+$(function () {
+
+    $("button.hamburger").on("click", function () {
+        $(this).toggleClass("is-active");
+    });
+
+});
+
+// JavaScript 版本
+document.addEventListener("DOMContentLoaded", function () {
+
+    var btn_hamburger = document.getElementsByClassName("hamburger")[0];
+    btn_hamburger.addEventListener("click", function () {
+        this.classList.toggle("is-active");
+    });
+
+});
+
+ // modal box
+ var modal = document.getElementById("myModal");
+ var btn = document.getElementById("myBtn");
+ var span = document.getElementsByClassName("close")[0];
+ btn.onclick = function () {
+     modal.style.display = "block";
+ }
+
+ span.onclick = function () {
+     modal.style.display = "none";
+ }
+ 
+ window.onclick = function (event) {
+     if (event.target == modal) {
+         modal.style.display = "none";
+     }
+ }
 
 
